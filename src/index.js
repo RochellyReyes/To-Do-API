fe0f55b2a17135c6c1b9b9e7e94090e535ef9a3b
Rochelly Reyes <rochelly.rhyna@gmail.com>
2021-02-06 23:01:11 -0500

    serve todo list app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
-
 const api = express();
-api.listen(3000, () =>{
-    console.log(`API running on port 3000`)
-});
+
+//middleware functionality  
+//https://expressjs.com/en/starter/static-files.html
+// api.use((req, res, next) => {
+//     console.log('Hello');
+//     next()
+// })
+api.use(express.static(__dirname + '/public'));
 
 api.get('/', (req, res) => {
     console.log(req);
     res.send('Hello World to-do-api')
+});
+
+
+
+
+
+
+
+api.listen(3000, () =>{
+    console.log(`API running on port 3000`)
 });
